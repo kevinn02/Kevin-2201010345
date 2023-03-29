b5f6d61e04aaa0e0f9c76f510013c55d1ed9dff2
kevinnpratamaa <116786857+kevinn02@users.noreply.github.com>
2023-03-29 09:41:52 +0800

    Add files via upload

---
+++ Sesi2/latihan02.php
@@ -0,0 +1,9 @@
+<?php
+    $angka1 = 5;
+    $angka2 = 9;
+    if($angka1 == $angka2){
+        echo "Nilai Vaiabel angka1 sama dengan Variabel angka2";
+    }
+    else{
+        echo "Nilai Variabel angka1 tidak sama dengan Variabel angka2";
+    }
---
+++ Sesi2/latihan04.php
@@ -0,0 +1,9 @@
+<?php
+$nAwal = 0;
+$nAkhir = 10;
+    while($nAwal < $nAkhir){
+        //blok di ulang
+        echo "Perulangan ke-".$nAwal."<br>";
+        $nAwal++;
+    }
+?>
